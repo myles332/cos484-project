e5dc716ccf8d28c3a1448c4659d9e0f33f66781a
myles332 <myles.anderson@princeton.edu>
2025-04-28 14:19:34 -0400

    config + run script

--- configs.py
+++ configs.py
@@ -1,5 +1,5 @@
 modeltype2path = {
-    "Llama-2-7b-chat-hf": "",
+    "Llama-2-7b-chat-hf": "meta-llama/Llama-2-7b-chat-hf",
     "Llama-2-13b-chat-hf": "",
     "Llama-2-7b-hf": "",
     "Llama-2-13b-hf": "",
