8c78c76c0dbdcdee48c8195a720447e58af558f6
Rayan Elahmadi <elahmadirayan@gmail.com>
2025-04-29 16:29:57 -0400

    Adding new model to configs.py

--- configs.py
+++ configs.py
@@ -1,6 +1,7 @@
 modeltype2path = {
-    "Llama-2-7b-chat-hf": "",
+    "Llama-2-7b-chat-hf": "meta-llama/Llama-2-7b-chat-hf",
     "Llama-2-13b-chat-hf": "",
     "Llama-2-7b-hf": "",
     "Llama-2-13b-hf": "",
+    "Llama-3-8b-hf": "meta-llama/Llama-3.1-8B-Instruct"
 }
