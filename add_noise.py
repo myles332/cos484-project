7dc06c2e9bcf5abf30545ce1e4576d524ff86357
myles332 <myles.anderson@princeton.edu>
2025-04-30 23:03:25 -0400

    add noise

--- add_noise.py
+++ add_noise.py
@@ -1,11 +1,12 @@
 import os
 import torch
+from random import random
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 # Config
 MODEL_ID = "meta-llama/Llama-2-7b-chat-hf"
 OUTPUT_DIR = "./llama2-noisy"
-NOISE_STDDEV = 0.01  # standard deviation of Gaussian noise
+NOISE_STDDEV = 0.0001  # standard deviation of Gaussian noise
 CACHE_DIR = os.getenv("HF_HOME")
 
 # Load model
@@ -18,7 +19,8 @@ model = AutoModelForCausalLM.from_pretrained(
 # Add noise to parameters
 with torch.no_grad():
     for param in model.parameters():
-        param.add_(torch.randn_like(param) * NOISE_STDDEV)
+        if random() < 0.1:
+            param.add_(torch.randn_like(param) * NOISE_STDDEV)
 
 # Save model with modified weights
 model.save_pretrained(OUTPUT_DIR)
